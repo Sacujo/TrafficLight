05db7fcf7258d1e5888015a675896cc2af4c0af4
Igor Guryan <sacujo93@yandex.ru>
2024-09-08 23:09:19 +0300

    add UI and logic

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,13 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+final class ViewController: UIViewController {
+    
+    enum CurrentTrafficLight {
+        case red, yellow, green
     }
 
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
+    @IBOutlet weak var changeLightButton: UIButton!
+    
+    let lightOff = 0.3
+    let lightOn = 1.0
+    
+    var currentTrafficLigh = CurrentTrafficLight.red
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        redLightView.layer.cornerRadius = 75
+        yellowLightView.layer.cornerRadius = 75
+        greenLightView.layer.cornerRadius = 75
+    }
+    @IBAction func changeLightButtonTapped(_ sender: UIButton) {
+        changeLightButton.setTitle("NEXT", for: .normal)
+        
+        switch currentTrafficLigh {
+        case .red:
+            redLightView.alpha = lightOn
+            greenLightView.alpha = lightOff
+            currentTrafficLigh = .yellow
+        case .yellow:
+            yellowLightView.alpha = lightOn
+            redLightView.alpha = lightOff
+            currentTrafficLigh = .green
+        case .green:
+            greenLightView.alpha = lightOn
+            yellowLightView.alpha = lightOff
+            currentTrafficLigh = .red
+        }
+    }
+    
 
 }
 
